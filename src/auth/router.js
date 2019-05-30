df82538316e19f959b240148bbd30753576d10b8
Joé Jemmely <joe.jemmely@gmail.com>
2019-05-30 14:56:29 -0700

    init submission

--- src/auth/router.js
+++ src/auth/router.js
@@ -9,14 +9,16 @@ const oauth = require('./oauth/google.js');
 
 authRouter.post('/signup', (req, res, next) => {
   let user = new User(req.body);
-  user.save()
-    .then( (user) => {
+  user
+    .save()
+    .then(user => {
       req.token = user.generateToken();
       req.user = user;
       res.set('token', req.token);
       res.cookie('auth', req.token);
       res.send(req.token);
-    }).catch(next);
+    })
+    .catch(next);
 });
 
 authRouter.post('/signin', auth, (req, res, next) => {
@@ -24,12 +26,21 @@ authRouter.post('/signin', auth, (req, res, next) => {
   res.send(req.token);
 });
 
-authRouter.get('/oauth', (req,res,next) => {
-  oauth.authorize(req)
-    .then( token => {
+authRouter.get('/oauth', (req, res, next) => {
+  oauth
+    .authorize(req)
+    .then(token => {
       res.status(200).send(token);
     })
     .catch(next);
+});
+
+authRouter.get('/protected-route', auth, (req, res, next) => {
+  res.status(200).send('asdf');
+});
+
+authRouter.post('/key', (req, res, next) => {
+  res.status(200);
 });
 
 module.exports = authRouter;
